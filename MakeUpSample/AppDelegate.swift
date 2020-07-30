4c177264292ec8f11de4bb368e7fc0a4b072bec6
toshiki tomihira <toshiki19970714@gmail.com>
2020-07-30 12:14:13 +0900

    Initial Commit

---
+++ MakeUpSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MakeUpSample
+//
+//  Created by 冨平準喜 on 2020/07/30.
+//  Copyright © 2020 冨平準喜. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MakeUpSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MakeUpSample
+//
+//  Created by 冨平準喜 on 2020/07/30.
+//  Copyright © 2020 冨平準喜. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
